5dbd9193162aa3b066b784f0e41b9f6b779da504
Ben Jansen <aogail@w007.org>
2017-10-23 17:12:56 -0400

    Fix TK on ubuntu; apt-get update

--- test/cookbooks/nagios_v_shell_test/metadata.rb
+++ test/cookbooks/nagios_v_shell_test/metadata.rb
@@ -6,6 +6,7 @@ description      'Integration test setup for nagios_v_shell'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'apt'
 depends 'nagios'
 depends 'zip'
 depends 'rsync'
--- test/cookbooks/nagios_v_shell_test/recipes/default.rb
+++ test/cookbooks/nagios_v_shell_test/recipes/default.rb
@@ -1,3 +1,4 @@
+include_recipe 'apt' if platform_family?('debian')
 include_recipe 'zip::default'
 include_recipe 'rsync::default'
 include_recipe 'nagios::default'
