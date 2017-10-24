68424244fd3c6ac96efae55c1315bdbb081b0e36
Ben Jansen <aogail@w007.org>
2017-10-24 15:51:48 -0400

    Version 0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aogail@w007.org'
 license 'MIT'
 description 'Installs/Configures nagios_v_shell'
 long_description 'Installs/Configures nagios_v_shell'
-version '0.2.1'
+version '0.2.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/aogail/chef-nagios-v-shell/issues'
 source_url 'https://github.com/aogail/chef-nagios-v-shell'
